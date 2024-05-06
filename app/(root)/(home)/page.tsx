3876a8d769080fa9b1d8668eac437cb9ea604576
Akash0601 <akash111shaw@gmail.com>
2024-05-06 12:31:29 +0530

    updated time format

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,8 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
 
   const now = new Date();
-
-  const time = now.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
+  const locale = 'en';
+  const time = now.toLocaleTimeString(locale, ( {hour: '2-digit', minute: '2-digit'}));
   const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
 
   return (
